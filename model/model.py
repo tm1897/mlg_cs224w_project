83b9465d0382b666c50c94738efe630bac3db289
eomeragic1 <eomeragic1@etf.unsa.ba>
2021-11-18 21:35:36 +0100

    Modified LightGCNStack and LightGCN classes

--- model/model.py
+++ model/model.py
@@ -6,54 +6,34 @@ import torch_scatter
 from torch_geometric.nn.conv import MessagePassing
 
 class LightGCNStack(torch.nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim, args, emb=False):
+    def __init__(self, latent_dim, args):
         super(LightGCNStack, self).__init__()
-        conv_model = self.build_conv_model(args.model_type)
+        conv_model = LightGCN
         self.convs = nn.ModuleList()
-        self.convs.append(conv_model(input_dim, hidden_dim))
+        self.convs.append(conv_model(latent_dim))
         assert (args.num_layers >= 1), 'Number of layers is not >=1'
-        for l in range(args.num_layers - 1):
-            self.convs.append(conv_model(args.heads * hidden_dim, hidden_dim))
-
-        # post-message-passing
-        self.post_mp = nn.Sequential(
-            nn.Linear(args.heads * hidden_dim, hidden_dim), nn.Dropout(args.dropout),
-            nn.Linear(hidden_dim, output_dim))
+        for l in range(args.num_layers-1):
+            self.convs.append(conv_model(latent_dim))
 
         self.dropout = args.dropout
         self.num_layers = args.num_layers
-
-        self.emb = emb
-
-    def build_conv_model(self, model_type):
-        return LightGCN
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
 
         for i in range(self.num_layers):
             x = self.convs[i](x, edge_index)
-            x = F.relu(x)
-            x = F.dropout(x, p=self.dropout, training=self.training)
 
-        x = self.post_mp(x)
-
-        if self.emb == True:
-            return x
-
-        return F.log_softmax(x, dim=1)
+        return x
 
     def loss(self, pred, label):
         return F.nll_loss(pred, label)
 
 
 class LightGCN(MessagePassing):
-    def __init__(self, latent_dim, dropout=0, **kwargs):
+    def __init__(self, latent_dim, **kwargs):
         super(LightGCN, self).__init__(node_dim=0, **kwargs)
-
         self.latent_dim = latent_dim
-        self.dropout = dropout
-
 
     def reset_parameters(self):
         nn.init.xavier_uniform_(self.lin_l.weight)
